22e473966bf7b6ffc3e5d745900871af0832baf8
dvdmarveira <ddmdoliveira@gmail.com>
2025-06-01 17:49:05 -0300

    feat: add home screen

---
+++ src/app/Home/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: 400,
+    color: "white",
+  },
+});
