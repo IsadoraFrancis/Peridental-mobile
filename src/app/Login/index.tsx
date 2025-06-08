f3ce55c952c7b03acf9d8db4483d03cd16262f9f
IsadoraFrancis <isadora.nizi@hotmail.com>
2025-06-08 14:21:35 -0300

    feat: Enhance AddCase screen and add new dependencies

--- src/app/Login/index.tsx
+++ src/app/Login/index.tsx
@@ -22,7 +22,7 @@ export function Login({ navigation }: any) {
         <MaterialIcons name="lock" size={24} color="#999"/>
         <TextInput style={{flex: 1}} placeholder='Senha'/>
       </View>
-        <TouchableOpacity style = {style.button} onPress={() => navigation.navigate('Home')}>
+        <TouchableOpacity style = {style.button} onPress={() => navigation.navigate('AddCase')}>
           <Text style={style.textButton} > Entrar </Text>
         </TouchableOpacity>
     </View>
