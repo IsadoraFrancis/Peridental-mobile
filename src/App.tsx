31098b8c4648911cea432369d955974ca493bce4
dvdmarveira <ddmdoliveira@gmail.com>
2025-06-09 00:44:10 -0300

    feat: adiciona rotas access e profile

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,21 @@ import { Hello } from "./app/Hello";
 import { Login } from "./app/Login";
 import { Home } from "./app/Home";
 import { AddCase } from "./app/AddCase";
-import { AddEvidence } from "./app/AddEvidence"
+import { AddEvidence } from "./app/AddEvidence";
+import { Access } from "./app/Access";
+import { Profile } from "./app/Access/Profile";
 
 const Stack = createNativeStackNavigator();
+
+export type RootStackParamList = {
+  Hello: undefined;
+  Login: undefined;
+  Home: undefined;
+  AddCase: undefined;
+  AddEvidence: undefined;
+  Access: undefined;
+  Profile: { user: { id: string; name: string; role: string; avatar: string } };
+};
 
 export function App() {
   return (
@@ -17,6 +29,8 @@ export function App() {
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="AddCase" component={AddCase} />
         <Stack.Screen name="AddEvidence" component={AddEvidence} />
+        <Stack.Screen name="Access" component={Access} />
+        <Stack.Screen name="Profile" component={Profile} />
       </Stack.Navigator>
     </NavigationContainer>
   );
